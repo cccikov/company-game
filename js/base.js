8fa44a6a3a5d292f04053ceadd8d3bd39878e44a
ccc <522978139@qq.com>
2017-06-27 17:50:07 +0800

    first day

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 function baseFontSize() {
-    var maxFontSize = 20;
-    var rootfontsize = window.innerWidth / 20;
+    var maxFontSize = 24;
+    var rootfontsize = window.innerWidth / 20; // 最宽就是20rem
     rootfontsize = rootfontsize > maxFontSize ? maxFontSize : rootfontsize;
     document.documentElement.style.fontSize = rootfontsize + "px"
     return rootfontsize;
